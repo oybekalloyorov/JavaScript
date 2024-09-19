4ee9465889e1f9a37a54ea6f6dd9ef02ebd27aec
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-09-19 21:08:19 +0500

    1 dars

---
+++ index.js
@@ -0,0 +1,45 @@
+// let name = "Akmalbek"; //String
+// let age = 16; // Number
+// let isMerried = false; // boolean
+// let firstName; //undefined
+
+// let lastName = null; //null
+
+// let sum = 27;
+// let yigindi = 89, kopaytma = "Hello", bolinma;
+
+// let a = 'Salom';
+// let A = 45;
+// sum = "Assalom";
+// console.log(sum);
+
+// const pi = 3.14;
+
+// console.log(pi); 
+
+// console.log( + )
+// console.log( - )
+// console.log( / )
+// console.log( * )
+// console.log( % )
+// console.log( ** )
+
+let a = 4;
+let b = 2;
+let sum = 68;
+
+ sum = a + b;
+
+console.log(sum);
+
+a = 45
+
+
+
+// console.log(a--);
+// console.log(a);
+ 
+
+
+
+
