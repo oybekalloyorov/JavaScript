2db735d4304cf3fe70cccc736c72fabab59c3d18
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-09-21 18:30:31 +0500

    2 dars

--- index.js
+++ index.js
@@ -39,6 +39,106 @@ a = 45
 // console.log(a--);
 // console.log(a);
  
+// ------------------------------------------------------------------------------
+// console.log("Salom");
+// + - * / % **
+// = += -= *= 
+
+// let son1 = 1 // number
+// let son = "1"; //String
+// console.log(son === son1);
+// >
+// < 
+// >=
+// <=
+// ==
+// ===
+// !=
+
+// &&, ||,  !
+
+// console.log(true && true && true);
+
+// let date = new Date();
+
+// let bosh_soat = date.getHours();
+// let tug_soat = date.getMinutes();
+// let sekund = date.getSeconds();
+// // let oqishVaqti = bosh_soat >= 15 && tug_soat <= 18;
+// console.log(bosh_soat + " " + tug_soat + " " +sekund);
+//  let x = 10;
+let y = 5;
+//  console.log(x > y);
+
+//  Agar x = 5 bolsa 
+// let x = "5";
+// console.log(x !== 5 );
+// console.log("2" == "12");
+
+
+// let a = "Akmalbek";
+// let b = null;
+// let c = undefined;
+// let d;
+// let e;
+// let f = a || b || c || d || e;
+// console.log(f);
+
+// console.log(Math.abs(-25)); // Sonning moduli
+// console.log(Math.acos(0.5));
+// console.log(Math.E); // Exponenta
+// console.log(Math.PI);
+// console.log(Math.LN10); 
+// console.log(Math.sqrt(100));
+// console.log(Math.floor(5.999999));
+// console.log(Math.ceil(7.1));
+// console.log(Math.log(10));
+// console.log(Math.max(2, 8, 10, 5));
+// console.log(Math.min(2, 8, 10, 5,1));
+
+// Shart Operatori
+
+// let a = 78;
+// let b = 55;
+// if(a % 2 == 0 || b % 2 == 0){
+//     a = "Juft son";
+// }else{
+//     a = "toq son";
+// }
+// console.log(a);
+// Vazifa
+// Agar berilgan son musbat bolsa uni 7 ga oshirilsin, 
+// son manfiy bolsa 9 ga kamaytirilsin
+// let son = -46;
+// if (son > 0){
+//     son += 7;
+// }else{
+//     son -= -9;
+// }
+// console.log(son);
+
+// Uchta son berilgan , Shu sonlarning yig'indisi eng katta boladigan 
+// ikkitasini ekranga chiqaring
+
+// let son1 = 8, son2 = 16, son3 = 9;
+
+// if((son1 + son2 > son1 + son3) && (son2 + son3 < son1 + son2)){
+//     console.log(son1 + ", " + son2);
+// }else {
+//     if((son1 + son3 > son2 + son1) && (son1 + son3 > son2 + son3)){
+//         console.log(son1 + ", " + son3);
+//     }else{
+//         if((son2 + son3 > son2 + son1) && (son2 + son3 > son1 + son3)){
+//             console.log(son2 + ", " + son3);
+//         }
+//     }
+// }
+let randomson = Math.floor(Math.random() * 1000);
+console.log(randomson);
+
+// 1 - 999 gacha sonlar berilgan. Shu oraliqdan bittasini olib, uni juft
+// yoki toq va Bir, ikki, uch xonali ekanligini ekranga chiqaring?
+
 
 
 
