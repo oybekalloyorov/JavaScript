f68869cc1025147880187e9624dcba36d3fcb05e
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-09-26 21:19:13 +0500

    dars4

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ let sum = 68;
 
  sum = a + b;
 
-console.log(sum);
+// console.log(sum);
 
 a = 45
 
@@ -134,7 +134,7 @@ let y = 5;
 //     }
 // }
 let randomson = Math.floor(Math.random() * 1000);
-console.log(randomson);
+// console.log(randomson);
 
 // 1 - 999 gacha sonlar berilgan. Shu oraliqdan bittasini olib, uni juft
 // yoki toq va Bir, ikki, uch xonali ekanligini ekranga chiqaring?
@@ -146,11 +146,111 @@ function summ(son1, son2,son3){
     return son1 + son2 + son3; 
 }
 
-console.log(summ(12,45,78));
-console.log(summ(1,69,85));
-console.log(summ(12,13,78));
-console.log(summ(1000,83,78));
-console.log(summ(12,1264,123));
+// console.log(summ(12,45,78));
+// console.log(summ(1,69,85));
+// console.log(summ(12,13,78));
+// console.log(summ(1000,83,78));
+// console.log(summ(12,1264,123));
 
 // vazifa
 // 1-100 Kiritilgan sonlarni matn holatiga o'tkazuvchi dastur 
+
+// dars 4
+
+
+// let ixtiyoriySon = 251;
+// let satr = "";
+// let yuzlik = Math.floor(ixtiyoriySon / 100);
+// let onlik = ixtiyoriySon % 100;
+// if (ixtiyoriySon >=200 && ixtiyoriySon < 300){
+//     satr += "ikki yuz "
+// }
+// if (ixtiyoriySon >=100 && ixtiyoriySon < 200){
+//     satr += "yuz "
+// }
+// if (ixtiyoriySon > 100){
+//     ixtiyoriySon = onlik;
+
+// }
+// if (ixtiyoriySon >=90 && ixtiyoriySon < 100){
+//     satr += "toqson "
+// }
+// if (ixtiyoriySon >=80 && ixtiyoriySon < 90){
+//     satr += "sakson "
+// }
+// if (ixtiyoriySon >=70 && ixtiyoriySon < 80){
+//     satr += "yetmish "
+// }
+// if (ixtiyoriySon >=60 && ixtiyoriySon < 70){
+//     satr += "oltmish "
+// }
+// if (ixtiyoriySon >=50 && ixtiyoriySon < 60){
+//     satr += "ellik "
+// }
+// if (ixtiyoriySon >=40 && ixtiyoriySon < 50){
+//     satr += "qirq "
+// }
+// if (ixtiyoriySon >=30 && ixtiyoriySon < 40){
+//     satr += "o'ttiz "
+// }
+// if (ixtiyoriySon >=20 && ixtiyoriySon < 30){
+//     satr += "yigirma "
+// }
+// if (ixtiyoriySon >=10 && ixtiyoriySon < 20){
+//     satr += "o'n "
+// }
+
+// if (ixtiyoriySon % 10 == 1){
+//     satr += "bir";
+// }else if (ixtiyoriySon % 10 == 2){
+//     satr += "ikki";
+// }else if (ixtiyoriySon % 10 == 3){
+//     satr += "uch";
+// }else if (ixtiyoriySon % 10 == 4){
+//     satr += "tort";
+// }else if (ixtiyoriySon % 10 == 5){
+//     satr += "besh";
+// }else if (ixtiyoriySon % 10 == 6){
+//     satr += "olti";
+// }else if (ixtiyoriySon % 10 == 7){
+//     satr += "yetti";
+// }else if (ixtiyoriySon % 10 == 8){
+//     satr += "sakkiz";
+// }else if (ixtiyoriySon % 10 == 9){
+//     satr += "toqqiz";
+// }
+
+// // console.log(satr);
+
+// // a, b va c butun sоnlar bеrilgan. Tеkshiring: a<b<c 
+// // tеngsizlik bajariladimi ? agar
+// //  tengsizlik bajarilsa “YES” aks xolda “NO” so’zini chiqaring.
+
+// let a1 =2;
+// let b1 = 4;
+// let c1 = 2;
+
+// if (a1 < b1 && b1 < c1){
+//     console.log("Yes");
+    
+// }else{
+//     console.log("No");
+    
+// }
+// // O`lchami x bo`lgan kubik berilgan uni hajmini toping.
+// let x = 10;
+// let kubHajmi = Math.pow(x,3);//x ** 3
+// // console.log(kubHajmi);
+
+// // Radiuslari r1, r2, r3 bo’lgan 3 
+// // to doira radiuslari berilgan. Doiralarni yuzlarinihisoblang.
+
+// let r1=4,r2=8,r3=6;
+
+// let doiraYuzi1 = r1 ** 2 * Math.PI;
+// let doiraYuzi2 = r2 ** 2 * Math.PI;
+// console.log("Birinchi doira yuzi: = ",doiraYuzi1);
+// console.log("Ikkinchi doira yuzi: = ",doiraYuzi2);
+
+
+
