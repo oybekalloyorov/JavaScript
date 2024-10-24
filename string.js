8d47773baea7fd3a27667afcc359d48674e77963
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-10-24 17:39:48 +0500

    takrorlash prompt, object, massiv ... va String mavzusiboshlandi

---
+++ string.js
@@ -0,0 +1,111 @@
+// let futbolName;
+// // console.log(typeof futbolCount);
+
+// // function startApp(){
+// //     futbolName = prompt("Jamoa nomini kiriting:");
+// //     while(futbolName == "" || futbolName == null){
+// //         futbolName = prompt("Jamoa nomini kiriting:");
+// //     }
+// // }
+// // startApp();
+
+// const futboll = {
+//     name: "",
+//     count: 10,
+//     score: [],
+//     players: {},
+//     stationName: "",
+// }
+// // console.log(futboll.name);
+
+// // let a = prompt("Birinchi jamoa nomini kiriting:"),
+// //     b = prompt("gollar sonini kiriting:");
+// // let c = prompt("Ikkinch jamoa nomini kiriting:"),
+// //     d = prompt("gollar sonini kiriting:");
+// // futboll.score[a] = b;
+// // futboll.score[c] = d;
+// // console.log(futboll);
+// for(let i = 0; i < 2; i++){
+//     let a = prompt(`Birinchi jamoa nomini kiriting:${i+1}`),
+//     b = prompt("gollar sonini kiriting:");
+
+//     if(a != "" && b != null && a != null && b != ""){
+//         futboll.score[a] = b;
+//     }else {
+//         i--
+//     }
+    
+// }
+// console.log(futboll);
+// String 
+// let text = "Oybek";
+// let arr = [1,2,3,4,5];
+
+// console.log(text[3] = "t");
+// console.log(arr[3] = "apple");
+// console.log(text);
+// console.log(arr);
+// let satr = "Hello world";
+// let upp = satr.toUpperCase()
+// console.log(satr.toLowerCase());
+// for(let i = 0; i < satr; i++){
+//     console.log(satr[i] , "=> ",i);
+    
+// }
+// console.log(satr.indexOf("t"));
+
+let CountOfF = 10;
+// function startApp(){
+//      CountOfF = +prompt("Gollar sonini kiriting");
+//     while(CountOfF == "" || CountOfF == null){
+//         CountOfF = +prompt("Gollar sonini kiriting");
+//     }
+// }
+// startApp();
+
+const futboll = {
+    count: CountOfF,
+    score: [],
+    players: {},
+
+}
+
+// let a = prompt("Birinchi jamoa nomini kiriting:"),
+//     b = prompt("gollar sonini kiriting:");
+// let c = prompt("Ikkinchi jamoa nomini kiriting:"),
+//     d = prompt("gollar sonini kiriting:");
+
+// for(let i = 0; i < 2; i++){
+//     let a = prompt(`jamoa nomini kiriting:${i+1}`);
+//     let b = prompt("gollar sonini kiriting:");
+//     if(a != "" && b != null && a != null && b != ""){
+//         futboll.score[a] = b;
+//     }else{
+//         i--;
+//     }
+    
+// }
+
+// let text = "Oybek";
+// let arr = [1,2,3,4,5];
+// console.log(text[2] = "t");
+// console.log(arr[2] = "Hello");
+// console.log(text);
+// console.log(arr);
+
+let txt = "Hello world!";
+// console.log(txt.toUpperCase());
+// console.log(txt.toLowerCase());
+console.log(txt.indexOf("w"));
+
+
+
+
+
+// futboll.score[a] = b;
+// futboll.score[c] = d;
+// console.log( futboll);
+
+let x = prompt("Ixtiyoriy sonni kiriting:");
+console.log(parseFloat(x));
+
