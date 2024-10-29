909976deb80c6a35c9ec3ddacd892bd1a9ed4114
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-10-29 16:30:08 +0500

    Callback function and string methods

---
+++ callbak.js
@@ -0,0 +1,56 @@
+// function first(){
+//     setTimeout(() => {
+//         console.log(1);
+//     },1000) 
+// }
+// function second(){
+//     console.log(2);
+// }
+// first();
+// second();
+
+// function edu(subject, cb){
+//     setTimeout(() =>{
+//         console.log(`I want ro study ${subject}`);
+//         cb();
+//     },1000)
+// }
+// function done(){
+//     console.log("That is great");
+// }
+// edu("JavaScript", done)
+
+// Samar Badriddinov
+// Farhod Dadajonov
+
+// String 
+let text = "Hi, My name is oybek and I live in khonqa";
+// console.log(text.match("bjkdfkjk"));
+// console.log(text.at(10));
+
+
+// console.log(text.startsWith("123"));
+// console.log(text.endsWith("rld"));
+
+
+// let satr = 'apple, grape'
+// let satr2= " Fruits";
+// let son = 12;
+// let son2 = 40;
+// console.log(text.concat(satr, satr2));
+
+// console.log(text.replace("World", "Water"));
+// console.log(text.split(" "));
+
+// console.log(text.repeat(10));
+
+
+// console.log( text.toLowerCase() );
+// console.log( text.toUpperCase() );
+// console.log(text.lastIndexOf("e"));
+// console.log(text.includes("hjhkjhj"));
+// console.log(text.slice(0,5));
+// console.log(text.trim());
+
+
+// "", 0, -1, false, 
