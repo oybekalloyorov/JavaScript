df8b2867960ccf8753ca0557cbff2bc86f4b6b51
oybekalloyorov <alloyorovoybek1997@gmail.com>
2024-10-31 16:11:41 +0500

    Dom (Document Object Model) 1 dars

---
+++ dom.js
@@ -0,0 +1,14 @@
+// const demo = document.getElementById("demo").innerHTML;
+
+// console.log(demo);
+// const tagName = document.getElementsByTagName("h1");
+// console.log(tagName[2].innerHTML);
+// console.log(tagName);
+
+// const clasName = document.getElementsByClassName("first");
+// console.log(clasName[0].innerHTML);
+
+// const demo1 = document.querySelectorAll(".cName");
+// console.log(demo1[3].innerHTML);
+// https://www.sammi.ac/
+// vazifa Sammi.ac dan vidiolarni korib kelish 24-27 videolar 
