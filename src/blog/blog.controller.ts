449827082be528c99b4acc7680d64446808a0bf4
oybekalloyorov <alloyorovoybek1997@gmail.com>
2025-11-15 15:14:19 +0500

    Service

--- src/blog/blog.controller.ts
+++ src/blog/blog.controller.ts
@@ -1,68 +1,40 @@
 import { Body, Controller, Delete, Get, HttpCode, Param, Patch, Post } from '@nestjs/common';
 import { BlogDto } from './dto/blog.dto';
+import { BlogService } from './blog.service';
 
 @Controller('blog')
 export class BlogController {
-    blogs: BlogDto[];
 
-    constructor() {
-        this.blogs = [
-            {
-                id: 1,
-                title: 'NestJS Blog Post',
-                excerpt: 'NestJs full Course',
-                description: 'Nest js Full Course from 0 to hero',
-            },
-            {
-                id: 2,
-                title: 'NextJS Blog Post',
-                excerpt: 'NextJs full Course',
-                description: 'Next js Full Course from 0 to hero',
-            },
-            {
-                id: 3,
-                title: 'NuxtJS Blog Post',
-                excerpt: 'NuxtJs full Course',
-                description: 'Nuxt js Full Course from 0 to hero',
-            }
-        ];
-    }
+    constructor(private readonly blogService: BlogService) { }
     @HttpCode(200)
     @Get()
     async getAll() {
-        return this.blogs;
+        return this.blogService.getAllBlogs();
     }
 
     @HttpCode(201)
     @Post()
     async create(@Body() dto: BlogDto) {
-        const data: BlogDto = {
-            ...dto,
-            id: new Date().getTime(),
-        };
-
-        return [...this.blogs, data];
+        return this.blogService.create(dto);
     }
     
 
     @HttpCode(200)
     @Get(":id")
     async getById(@Param("id") id: string) {
-        return this.blogs.find((item) => item.id === Number(id));
+        return this.blogService.getById(id);
     }
 
     @HttpCode(200)
     @Patch(":id")
     async update(@Param("id") id: string, @Body() dto: BlogDto) {
-        let currentBlog = await this.blogs.find((item) => item.id === Number(id));
-        currentBlog = dto;
-        return currentBlog;
+        return this.blogService.update(id, dto);
     }
 
     @HttpCode(200)
     @Delete(":id")
     async delete(@Param("id") id: string) {
-        return this.blogs.filter((item) => item.id !== Number(id));
+       return this.blogService.delete(id);
     }
 
 }
--- src/blog/blog.service.ts
+++ src/blog/blog.service.ts
@@ -1,4 +1,55 @@
 import { Injectable } from '@nestjs/common';
+import { BlogDto } from './dto/blog.dto';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+    blogs: BlogDto[];
+    constructor() {
+            this.blogs = [
+                {
+                    id: 1,
+                    title: 'NestJS Blog Post',
+                    excerpt: 'NestJs full Course',
+                    description: 'Nest js Full Course from 0 to hero',
+                },
+                {
+                    id: 2,
+                    title: 'NextJS Blog Post',
+                    excerpt: 'NextJs full Course',
+                    description: 'Next js Full Course from 0 to hero',
+                },
+                {
+                    id: 3,
+                    title: 'NuxtJS Blog Post',
+                    excerpt: 'NuxtJs full Course',
+                    description: 'Nuxt js Full Course from 0 to hero',
+                }
+            ];
+        }
+    async getAllBlogs() {
+        return this.blogs;
+    }
+
+    async create(dto: BlogDto) {
+        const data: BlogDto = {
+            ...dto,
+            id: new Date().getTime(),
+        };
+
+        return [...this.blogs, data];
+    }
+
+    async getById(id: string) {
+        return this.blogs.find((item) => item.id === Number(id));
+    }
+
+    async update(id: string, dto: BlogDto) {
+        let currentBlog = await this.blogs.find((item) => item.id === Number(id));
+        currentBlog = dto;
+        return currentBlog;
+    }
+
+    async delete(id: string) {
+         return this.blogs.filter((item) => item.id !== Number(id));
+    }
+}
